ca1735057ae219a561fe41f428bc6fabaadae2e4
Quinn Qiu <quinnqiu0823@icloud.com>
2023-10-06 23:44:41 +0800

    Update project.js

--- apple-supports-community-posting-tools/js/project.js
+++ apple-supports-community-posting-tools/js/project.js
@@ -14,7 +14,7 @@ select.options[2].selected = true;
 
 /* --- 默认文本 --- */
 /* 版权声明 */
-var copyrightOut = "<p><br>由 Quinn0823 书写。</p>";
+/* var copyrightOut = "<p><br>由 Quinn0823 书写。</p>"; */
 /* 步骤 */
 document.getElementById("stepOut").innerHTML = "<br><p>尝试以下操作步骤：<p>";
 
@@ -24,19 +24,19 @@ var stepOut = document.getElementById("stepOut");
 var stepList = document.createElement("ol");
 stepOut.appendChild(stepList);
 
-document.getElementById("copyrightOut").innerHTML = copyrightOut;
+/* document.getElementById("copyrightOut").innerHTML = copyrightOut; */
 
 typeChange();
 function typeChange() {
     choose = select.selectedIndex;
     type = select.options[choose].text;
-    var endingOut = "<br><p>如果以上没能很好地解决你的问题，你可以联系 Apple 支持以获取更多支持。只需要点击以下链接，并选择“" + type + "”：<a href='https://getsupport.apple.com/products'>Apple Support</a>。</p>";
+    var endingOut = "<br><p>如果以上没能很好地解决你的问题，你可以联系 Apple 以获取更多协助。只需要点击以下链接，并选择“" + type + "”：<a href='https://getsupport.apple.com/products'>Apple Support</a>。希望可以帮助到你，谢谢！</p>";
     document.getElementById("endingOut").innerHTML = endingOut;
 }
 
 probChange();
 function probChange() {
-    var probOut = "<p>针对你在 " + type + " 上遇到的“" + document.getElementById("probIn").value + "”的问题，你可以：</p>";
+    var probOut = "<p>针对你在 " + type + " 上遇到的“" + document.getElementById("probIn").value + "”问题，你可以：</p>";
     document.getElementById("probOut").innerHTML = probOut;
 }
 
