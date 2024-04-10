d306f189276e7c83720e0c2bbc007c15ad300f61
Quinn Qiu <quinnqiu0823@icloud.com>
2024-04-10 18:31:32 +0800

    Update page.js

--- base/js/page.js
+++ base/js/page.js
@@ -1,7 +1,10 @@
 var title = document.title;
 
+var update = document.getElementById("update").innerHTML;
+var people = document.getElementById("people").innerHTML
+
 var header = "<h1>" + title + "</h1><hr>";
-var footer = "<hr><p class='copyright'>Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " " + document.getElementById("footer").innerHTML + " All Rights Reserved. | <a href='https://quinn0823.github.io/'  target='_blank'>Quinn-Web</a></p>";
+var footer = "<hr><p class='footer'>📅 Last Update: " + update + " | Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + " " + people + " All Rights Reserved. | <a href='https://quinn0823.github.io/' target='_blank'>Quinn-Web</a></p>";
 
 document.getElementById("header").innerHTML = header;
 document.getElementById("footer").innerHTML = footer;
