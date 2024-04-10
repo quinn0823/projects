5ab16b572c5ac17ff59526fb19ee7f4b9569dfee
Quinn Qiu <quinnqiu0823@icloud.com>
2024-04-10 18:23:04 +0800

    Update page.js

--- base/js/page.js
+++ base/js/page.js
@@ -1,7 +1,7 @@
 var title = document.title;
 
 var header = "<h1>" + title + "</h1><hr>";
-var footer = "<hr><p class='copyright'>Copyright © 2023 " + document.getElementById("footer").innerHTML + " All Rights Reserved.</p>";
+var footer = "<hr><p class='copyright'>Copyright © 2019-" + new Date(Date.parse(new Date())).getFullYear() + document.getElementById("footer").innerHTML + " All Rights Reserved. | <a href='https://quinn0823.github.io/'  target='_blank'>Quinn-Web</a></p>";
 
 document.getElementById("header").innerHTML = header;
 document.getElementById("footer").innerHTML = footer;
