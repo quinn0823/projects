a80b53fd4cf2541e3e98b29d5b525e9df4574468
Quinn0823 <quinnqiu0823@icloud.com>
2024-04-14 10:06:44 +0800

    Update page.js

--- js/page.js
+++ js/page.js
@@ -9,3 +9,20 @@ var footer = "<hr><p class='footer'>Last Updated: " + update + " | Copyright © 
 
 document.getElementById("header").innerHTML = header;
 document.getElementById("footer").innerHTML = footer;
+
+var clipboard = new ClipboardJS('.copy_text');
+
+clipboard.on('success', function(e) {
+    alert(e.text + "has been copied.");
+
+    // console.info('Action:', e.action);
+    // console.info('Text:', e.text);
+    // console.info('Trigger:', e.trigger);
+
+    // e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    // console.error('Action:', e.action);
+    // console.error('Trigger:', e.trigger);
+});
