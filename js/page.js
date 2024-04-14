7bf4583022d129cf1ebb85f7dae4c5e577b436eb
Quinn0823 <quinnqiu0823@icloud.com>
2024-04-14 13:37:31 +0800

    Update page.js

--- js/page.js
+++ js/page.js
@@ -1,11 +1,12 @@
 var title = document.title;
+var path = window.location.pathname;
 
 var update = document.getElementById("update").innerHTML;
 var year = document.getElementById("year").innerHTML;
 var people = document.getElementById("people").innerHTML;
 
 var header = "<h1>" + title + "</h1><hr>";
-var footer = "<hr><p class='footer'>Last Updated: " + update + " | Copyright © " + year + " " + people + " All Rights Reserved. | <a href='https://quinn0823.github.io/' target='_blank'>Quinn-Web</a></p>";
+var footer = "<hr><p class='footer' id='footer_p'>Last Updated: " + update + " | Copyright © " + year + " " + people + " All Rights Reserved.</p><p class='footer'><a id='footer_a1' href='https://quinn0823.github.io/' target='_blank'>Quinn-Web</a> | <a id='footer_a2' href='https://quinn0823.github.io'" + path + " target='_blank'>GitHub</a></p>";
 
 document.getElementById("header").innerHTML = header;
 document.getElementById("footer").innerHTML = footer;
