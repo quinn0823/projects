b14cbe845852fb6129b5d7302c5fb5908d767da7
Quinn0823 <quinnqiu0823@icloud.com>
2024-04-14 14:00:37 +0800

    Update page.js

--- js/page.js
+++ js/page.js
@@ -6,7 +6,7 @@ var year = document.getElementById("year").innerHTML;
 var people = document.getElementById("people").innerHTML;
 
 var header = "<h1 id='header_h1'>" + title + "</h1><hr>";
-var footer = "<hr><p class='footer' id='footer_p'>Last Updated: " + update + " | Copyright © " + year + " " + people + " All Rights Reserved.</p><p class='footer'><a id='footer_a1' href='https://quinn0823.github.io/' target='_blank'>Quinn-Web</a> | <a id='footer_a2' href='https://quinn0823.github.io'" + path + " target='_blank'>GitHub</a></p>";
+var footer = "<hr><p class='footer' id='footer_p'>Last Updated: " + update + " | Copyright © " + year + " " + people + " All Rights Reserved.</p><p class='footer'><a id='footer_a1' href='https://quinn0823.github.io/' target='_blank'>Quinn-Web</a> | <a id='footer_a2' href='https://github.com/Quinn0823/" + path + "' target='_blank'>GitHub</a></p>";
 
 document.getElementById("header").innerHTML = header;
 document.getElementById("footer").innerHTML = footer;
